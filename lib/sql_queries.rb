c827469897ac159f07efd427db6fdc4605e2c6df
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-18 03:08:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   #"SELECT color, COUNT(color) from bears GROUP BY color"
-  "SELECT * FROM bears"
+  "SELECT color FROM bears"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
