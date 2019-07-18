ca5fbea2d219b1a2d16253a6f8beca346032c65f
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-18 02:55:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,10 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) from bears GROUP BY color"
+  "SELECT MAX (max color)
+   FROM (SELECT color, max (color) 
+   FROM bears 
+   GROUP BY color)"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
